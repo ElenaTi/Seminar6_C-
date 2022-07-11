e2388d8c26efb90e43889590a51502e3873a4eea
Elena Timofeeva <posazho@gmail.com>
2022-07-11 21:27:01 +0300

    перевод числа в двоичную систему счисления

---
+++ Seminar6_C#/Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10 */
+
+
+string GetBinary (int A)
+{
+    string result = String.Empty;
+    while (A > 0)
+    {
+        int number2 = A % 2;
+        A = A / 2; 
+        result = number2 + result;     
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string number1 = GetBinary(number);
+Console.WriteLine(number1);
