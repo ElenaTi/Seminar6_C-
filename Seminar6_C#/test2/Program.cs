bef274b47069d05801bd74bb99c1c5edefc49c38
Elena Timofeeva <posazho@gmail.com>
2022-07-14 11:07:50 +0300

    для понимания, как работает concat.toarray

---
+++ Seminar6_C#/test2/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[] { 3, 4, 5,85 };
+
+array = array.Concat(new int[] { 10, 12 }).ToArray();
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    Console.Write("[");
+    while (index < count)
+    {
+        Console.Write(coll[index]);
+        index++;
+        if (index < count)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+PrintArray(array);
+
