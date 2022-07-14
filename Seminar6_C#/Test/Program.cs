d08646016e71e14d66e40303d8f6c93de0dfa5db
Elena Timofeeva <posazho@gmail.com>
2022-07-14 11:21:26 +0300

    ввод чисел через запятую, поиск четных чисел

---
+++ Seminar6_C#/Test/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("Введите числа через запятую.");
+string? Numbers = Console.ReadLine();
+Numbers = Numbers + ",";
+int[] array = Array(Numbers);
+PrintArray(array);
+Console.WriteLine("");
+int positiveNumbers = CountPositiveNumbers(array);
+Console.WriteLine($"Было введено {positiveNumbers} положительных чисел");
+
+int[] Array(string text)
+{
+    int[] array = new int[1];
+
+    int index = 0;
+    int col = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        string separateNumbers = String.Empty;
+
+        while (text[i] != ',' && i < text.Length)
+        {
+            separateNumbers = separateNumbers + text[i];
+            i++;
+        }
+        array[index] = Convert.ToInt32(separateNumbers);
+        // Console.WriteLine(array[index]);
+        if (i < text.Length - 1)
+        {        
+            array = array.Concat(new int[] { 0 }).ToArray();        
+        } 
+        index++;      
+        // Console.WriteLine($"index = {index}");
+    }
+    return array;
+}
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    Console.Write("[");
+    while (index < count)
+    {
+        Console.Write(coll[index]);
+        index++;
+        if (index < count)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int CountPositiveNumbers(int[] massive)
+{
+    int count =0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
